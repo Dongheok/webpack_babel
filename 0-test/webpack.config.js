284592422c6ede5548daf2f78d211cee239392cf
dongheok <piouy_@naver.com>
2020-12-04 01:12:36 +0900

    ## 로더 실습 전

--- 0-test/webpack.config.js
+++ 0-test/webpack.config.js
@@ -9,4 +9,30 @@ module.exports = {
       path: path.resolve('./dist'),
       filename: '[name].js',
    },
+   // 로더
+   module: {
+      rules: [
+         // js 로더
+         // {
+         //    test: /\.js$/,
+         //    use: [
+         //       path.resolve('./my-webpack-loader.js')
+         //    ],
+         // },
+         // css 로더, style 로더, file 로더
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'url-loader',
+            options: {
+               publicPath: './dist/',
+               name: '[name].[ext]?[hash]',
+               limit: 20000, // 2kb
+            },
+         },
+      ],
+   },
 };
